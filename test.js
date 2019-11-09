4802938ea00935d6f90f3a422d8a6fcb40a5ccde
Matthew <matthewleemle@gmail.com>
2019-11-09 14:23:32 -0800

    update

--- test.js
+++ test.js
@@ -1,22 +1,24 @@
 const fetch = require("node-fetch");
 
+var origin = "999 Canada Pl, Vancouver, BC V6C 3T4";
+var destination = "10153 King George Blvd, Surrey, BC V3T 2W1";
 
-var origin = "49.287751,-123.1139";
-var destination = "48.2820,-123.1171";
 
-getTransitJSON(origin,destination)
+var json = getTransitData(origin,destination)
   .then(json=>console.log(json));
 
-//
-// async function getTransitData(){
-//
-//   var origin = "999 Canada Pl, Vancouver, BC V6C 3T4";
-//   var destination = "10153 King George Blvd, Surrey, BC V3T 2W1";
-//
-//
-//   return 0;
-//
-// }
+
+async function getTransitData(){
+  var coordStrOrigin = await getcoordinatesJSON(origin);
+  var coordStrDest = await getcoordinatesJSON(destination);
+
+  var data = await getTransitJSON(coordStrOrigin,coordStrDest);
+
+  return data;
+
+  return 0;
+
+}
 
 
 
@@ -25,7 +27,7 @@ async function getTransitJSON(origin, destination){
     let response = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&mode=transit&key=AIzaSyCWle8TgNqMNLNqj3mdeXmkU7ej0xfZAyA`);
     let data = await response.json();
 
-
+    
       let transitData = {
           "duration" : data.routes[0].legs[0].duration.text,
           "distance" : data.routes[0].legs[0].distance.text
