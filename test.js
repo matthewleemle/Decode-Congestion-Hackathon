a50abe18d004938f6bffb51f9d6e01557be51b64
Matthew <matthewleemle@gmail.com>
2019-11-09 13:32:45 -0800

    ver1

--- test.js
+++ test.js
@@ -2,28 +2,17 @@ const fetch = require("node-fetch");
 
 
 var route;
+var origin1 = "999 Canada Pl, Vancouver, BC V6C 3T4";
+var destination1 = "10153 King George Blvd, Surrey, BC V3T 2W1";
 
-var origin = "49.28,-123.1171";
+var origin = getcoordinatesJSON(origin) ;
 var destination = "49.166,-123.136";
 
 var json = getTransitJSON(origin,destination)
   .then(json=>console.log(json));
 
 
-async function getBikingJSON(origin, destination){
 
-  let response = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&mode=bicycling&key=AIzaSyCWle8TgNqMNLNqj3mdeXmkU7ej0xfZAyA`);
-  let data = await response.json();
-
-  var json = {
-     "duration" : data.routes[0].legs[0].duration.text,
-     "distance" : data.routes[0].legs[0].distance.text,
-   };
-
-
-  return json;
-
-}
 
 async function getTransitJSON(origin, destination){
 
